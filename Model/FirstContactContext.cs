4020df75329ae11a7902e1b8ccd6ec131a15ed6d
Allen Cardoso <allencardoso@bemol.com.br>
2023-05-21 21:20:34 -0400

    Alterado controller e repository, nova classe CandidateData criada

--- Model/FirstContactContext.cs
+++ Model/FirstContactContext.cs
@@ -4,11 +4,16 @@ namespace FirstContactAPI.Model
 {
     public class FirstContactContext : DbContext
     {
+        public DbSet<FirstContact> FirstContacts { get; set; }
+
         public FirstContactContext(DbContextOptions<FirstContactContext> options) : base(options)
-        { 
-            Database.EnsureCreated(); 
+        {
+            Database.EnsureCreated();
         }
 
-        public DbSet<FirstContact> FirstContacts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=test.sqlite");
+        }
     }
 }
